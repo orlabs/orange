e3e64a8948597f7cb3806b8d1a0a833626fdcf55
sumory <sumory.wu@gmail.com>
2016-04-10 23:43:58 +0800

    support variable extraction for redirect plugin

--- orange/plugins/redirect/handler.lua
+++ orange/plugins/redirect/handler.lua
@@ -1,7 +1,11 @@
 local pairs = pairs
 local string_len = string.len
 local string_find = string.find
+local ngx_re_gsub = ngx.re.gsub
+local ngx_redirect = ngx.redirect
 local judge_util = require("orange.utils.judge")
+local extractor_util = require("orange.utils.extractor")
+local handle_util = require("orange.utils.handle")
 local BasePlugin = require("orange.plugins.base")
 
 
@@ -22,9 +26,7 @@ function RedirectHandler:redirect()
 
     local redirect_rules = redirect_config.redirect_rules
 
-    local ngx_set_uri = ngx.req.set_uri
-    local ngx_re_gsub = ngx.re.gsub
-    local ngx_redirect = ngx.redirect
+
     local ngx_var = ngx.var
     local ngx_var_uri = ngx_var.uri
     local ngx_var_scheme = ngx_var.scheme
@@ -33,47 +35,52 @@ function RedirectHandler:redirect()
     for i, rule in pairs(redirect_rules) do
         local enable = rule.enable
         if enable == true then
+
+            -- judge阶段
             local judge = rule.judge
-            local match_type = judge.type
+            local judge_type = judge.type
             local conditions = judge.conditions
             local pass = false
-            if match_type == 0 or match_type == 1 then
+            if judge_type == 0 or judge_type == 1 then
                 pass = judge_util.filter_and_conditions(conditions)
-            elseif match_type == 2 then
+            elseif judge_type == 2 then
                 pass = judge_util.filter_or_conditions(conditions)
-            elseif match_type == 3 then
+            elseif judge_type == 3 then
                 pass = judge_util.filter_complicated_conditions(judge.expression, conditions, self:get_name())
             end
 
+            -- extract阶段
+            local extractor = rule.extractor
+            local extractions = extractor and extractor.extractions
+            local variables
+            if extractions then
+                variables = extractor_util.extract(extractions)
+            end
+
+            -- handle阶段
             if pass then
                 local handle = rule.handle
-
-                if handle and handle.redirect_to then
-                    local new_url
-                    local replace_re = handle.regrex
-                    if replace_re and replace_re ~= "" then
-                        new_url = ngx_re_gsub(ngx_var_uri, replace_re, handle.redirect_to)
-                    else
-                        new_url = handle.redirect_to
-                    end
-
+                if handle and handle.url_tmpl then
+                    local new_url = handle_util.build_url(handle.url_tmpl, variables, self:get_name())
                     if new_url ~= ngx_var_uri then
+                        local redirect_status = handle.redirect_status
+                        -- ngx.HTTP_MOVED_PERMANENTLY (301)
+                        -- ngx.HTTP_MOVED_TEMPORARILY (302)
+
                         if string_find(new_url, 'http') ~= 1 then
-                            new_url = ngx_var_scheme.."://"..ngx_var_host..new_url
+                            new_url = ngx_var_scheme .. "://" .. ngx_var_host .. new_url
                         end
 
                         if ngx_var.args ~= nil then
-                            ngx_redirect( new_url.."?"..ngx_var.args , ngx.HTTP_MOVED_TEMPORARILY)
-                        else
-                            ngx_redirect( new_url , ngx.HTTP_MOVED_TEMPORARILY)
+                            new_url = new_url .. "?" .. ngx_var.args
                         end
+
+                        ngx_redirect(new_url, redirect_status or ngx.HTTP_MOVED_TEMPORARILY)
 
                         if handle.log == true then
-                            ngx.log(ngx.ERR, "[Redirect] ", ngx_var_uri, " to:",  new_url)
+                            ngx.log(ngx.ERR, "[Redirect] ", ngx_var_uri, " to:", new_url)
                         end
-
                     end
-
                     return
                 end
             end
