c7285f9ebe9305db891c1a6f9ff155a23e1de69d
sumory <sumory.wu@gmail.com>
2016-04-14 12:26:41 +0800

    recognized global status monitor code

--- dashboard/static/js/status.js
+++ dashboard/static/js/status.js
@@ -107,15 +107,25 @@
                             trafficOption.xAxis[0].data.push(axisData);
                             _this.data.trafficChart.setOption(trafficOption);
 
-                            for(var i=2;i<=5;i++){
-                                var option = _this.data.stateChart[i+"xx"].getOption();
-                                data0 = option.series[0].data;
-                                data0.shift();
-                                data0.push( data["request_" + i + "xx"]);
-                                option.xAxis[0].data.shift();
-                                option.xAxis[0].data.push(axisData);
-                                _this.data.stateChart[i+"xx"].setOption(option);
-                            }
+                            //HTTP status统计
+                            var stateOption = _this.data.stateChart.getOption();
+                            data0 = stateOption.series[0].data;
+                            data0.shift();
+                            data0.push( data["request_2xx"]);
+
+                            data1 = stateOption.series[1].data;
+                            data1.shift();
+                            data1.push( data["request_3xx"]);
+
+                            var data2 = stateOption.series[2].data;
+                            data2.shift();
+                            data2.push( data["request_4xx"]);
+
+                            var data3 = stateOption.series[3].data;
+                            data3.shift();
+                            data3.push( data["request_5xx"]);
+                            _this.data.stateChart.setOption(stateOption);
+
 
 	                    }else{
 	                        APP.Common.showTipDialog("错误提示", result.msg);
@@ -146,8 +156,15 @@
         	var option = {
 	            title : {
 	                text: '请求统计',
-	                subtext: ''
+	                subtext: '',
+                    left:'10px'
 	            },
+                grid: {
+                    left: '20px',
+                    right: '20px',
+                    bottom: '30px',
+                    containLabel: true
+                },
 	            tooltip : {
 	                trigger: 'axis'
 	            },
@@ -156,6 +173,7 @@
 	            },
 	            toolbox: {
 	                show: true,
+                    right:"20px",
 			        feature: {
 			            dataView: {readOnly: false},
 			            saveAsImage: {}
@@ -188,7 +206,11 @@
 	                {
 	                    name:'全部请求',
 	                    type:'line',
-	                    smooth: true,
+                        itemStyle: {
+                            normal: {
+                                color: '#03A1F7'
+                            }
+                        },
 	                    data:(function (){
 	                        var res = [];
 	                        var len = 100;
@@ -202,8 +224,11 @@
 	                {
 	                    name:'失败请求',
 	                    type:'line',
-	                    itemStyle: {normal: {areaStyle: {type: 'default'}}},
-	                    symbol: 'none',
+                        itemStyle: {
+                            normal: {
+                                color: '#F75903'
+                            }
+                        },
 	                    data:(function (){
 	                        var res = [];
 	                        var len = 100;
@@ -225,8 +250,15 @@
         	var option = {
 	            title : {
                     text: 'QPS统计',
-                    subtext: ''
+                    subtext: '',
+                    left:'26px'
 	            },
+                grid: {
+                    left: '33px',
+                    right: '33px',
+                    bottom: '30px',
+                    containLabel: true
+                },
 	            tooltip : {
 	                trigger: 'axis'
 	            },
@@ -235,6 +267,7 @@
 	            },
 	             toolbox: {
                      show: true,
+                     right:"34px",
                      feature: {
                          dataView: {readOnly: false},
                          saveAsImage: {}
@@ -267,8 +300,12 @@
 	                {
 	                    name:'QPS',
 	                    type:'line',
-	                    smooth: true,
-	                    symbol: 'none',
+                        itemStyle: {
+                            normal: {
+                                color: '#ECA047'
+                            }
+                        },
+                        areaStyle: {normal: {}},
 	                    data:(function (){
 	                        var res = [];
 	                        var len = 100;
@@ -290,20 +327,20 @@
         	var option = {
 	            title : {
                     text: '请求时间统计',
+                    left:'10px',
                     subtext: ''
 	            },
+                grid: {
+                    left: '15px',
+                    right: '10px',
+                    bottom: '30px',
+                    containLabel: true
+                },
 	            tooltip : {
 	                trigger: 'axis'
 	            },
 	            legend: {
 	                data:['响应时间']
-	            },
-	             toolbox: {
-                     show: true,
-                     feature: {
-                         dataView: {readOnly: false},
-                         saveAsImage: {}
-                     }
 	            },
 	            xAxis : [
 	                {
@@ -357,18 +394,17 @@
                     text: '流量统计',
                     subtext: ''
 	            },
+                grid: {
+                    left: '15px',
+                    right: '10px',
+                    bottom: '30px',
+                    containLabel: true
+                },
 	            tooltip : {
 	                trigger: 'axis'
 	            },
 	            legend: {
 	                data:['in','out']
-	            },
-	             toolbox: {
-                     show: true,
-                     feature: {
-                         dataView: {readOnly: false},
-                         saveAsImage: {}
-                     }
 	            },
 	            xAxis : [
 	                {
@@ -429,71 +465,103 @@
         },
 
         initStateStatus: function(code){
-        	var color = "rgb(182, 207, 220)";
-        	if(code == "4xx" || code == "5xx"){
-        		color = "rgb(238, 109, 68)";
-        	}
-
-        	var option = {
-	            title : {
-                    text: code + '请求统计',
+            var option = {
+                title : {
+                    text: 'HTTP Status 统计',
+                    left:'26px',
                     subtext: ''
-	            },
-	            tooltip : {
-	                trigger: 'axis'
-	            },
-	            legend: {
-	                data:[code+"请求"]
-	            },
-	             toolbox: {
-                     show: true,
-                     feature: {
-                         dataView: {readOnly: false},
-                         saveAsImage: {}
-                     }
-	            },
-	            xAxis : [
-	                {
-	                    type : 'category',
-	                    boundaryGap : false,
-	                    data : (function (){
-	                        var now = new Date();
-	                        var res = [];
-	                        var len = 50;
-	                        while (len--) {
-	                            res.unshift(APP.Common.formatTime());
-	                            now = new Date(now - 400);
-	                        }
-	                        return res;
-	                    })()
-	                }
-	            ],
-	            yAxis : [
-	                {
-	                    type : 'value',
-	                    scale: true,
-	                    name : '次'
-	                }
-	            ],
-	            series : [
-	                {
-	                    name:'请求',
-	                    type:'line',
-	                    data:(function (){
-	                        var res = [];
-	                        var len = 50;
-	                        while (len--) {
-	                          res.push(0);
-	                        }
-	                        return res;
-	                    })()
-	                }
-	            ]
-	        };
+                },
+                grid: {
+                    left: '36px',
+                    right: '30px',
+                    bottom: '30px',
+                    containLabel: true
+                },
+                tooltip : {
+                    trigger: 'axis',
+                    axisPointer : {// 坐标轴指示器，坐标轴触发有效
+                        type : 'shadow'// 默认为直线，可选为：'line' | 'shadow'
+                    }
+                },
+                legend: {
+                    data:['2xx请求', '3xx请求','4xx请求','5xx请求']
+                },
+                xAxis : [
+                    {
+                        type : 'value'
+                    }
+                ],
+                yAxis : [
+                    {
+                        type : 'category',
+                        axisTick : {show: false},
+                        data : ['']
+                    }
+                ],
+                series : [
+                    {
+                        name:'2xx请求',
+                        type:'bar',
+                        label: {
+                            normal: {
+                                show: true,
+                                position: 'inside'
+                            }
+                        },
+                        itemStyle: {
+                            normal: {
+                                color: '#269EBD'
+                            }
+                        },
+                        data:[0]
+                    },
+                    {
+                        name:'3xx请求',
+                        type:'bar',
+                        label: {
+                            normal: {
+                                show: true
+                            }
+                        },
+                        data:[0]
+                    },
+                    {
+                        name:'4xx请求',
+                        type:'bar',
+                        label: {
+                            normal: {
+                                show: true
+                            }
+                        },
+                        itemStyle: {
+                            normal: {
+                                color: '#B94D23'
+                            }
+                        },
+                        data:[0]
+                    },
+                    {
+                        name:'5xx请求',
+                        type:'bar',
+                        label: {
+                            normal: {
+                                show: true
+                            }
+                        },
+                        itemStyle: {
+                            normal: {
+                                color: '#F75903'
+                            }
+                        },
+                        data:[0]
+                    },
 
-	        var chart = echarts.init(document.getElementById(code+'-area'));
+
+                ]
+            };
+	        var chart = echarts.init(document.getElementById('status-area'));
 	        chart.setOption(option);
-	        _this.data.stateChart[code] = chart;
+	        _this.data.stateChart = chart;
         },
 
     };
