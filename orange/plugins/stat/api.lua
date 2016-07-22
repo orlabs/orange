833e42aa9749694b9662621cfdad7d5cff81c75a
sumory <sumory.wu@gmail.com>
2016-07-22 16:26:18 +0800

    modify stat plugin api as normal format

--- orange/plugins/stat/api.lua
+++ orange/plugins/stat/api.lua
@@ -2,15 +2,19 @@ local stat = require("orange.plugins.stat.stat")
 
 local API = {}
 
-API["/stat/status"] = function(req, res, next)
-    local stat_result = stat.stat()
+API["/stat/status"] = {
+    GET = function(store)
+	    return function(req, res, next)
+		    local stat_result = stat.stat()
 
-    local result = {
-        success = true,
-        data = stat_result
-    }
+		    local result = {
+		        success = true,
+		        data = stat_result
+		    }
 
-    res:json(result)
-end
+		    res:json(result)
+		end
+	end
+}
 
 return API
