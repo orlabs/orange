966f8badb961f381869d91e953c132b12bd18da7
fish <fishioon@live.com>
2016-08-23 17:03:49 +0800

    check nginx install

--- start.sh
+++ start.sh
@@ -8,15 +8,22 @@ then
 	pid=`cat $current_path/logs/nginx.pid`
 fi
 
+nginx=`which nginx`
+if [ $? -ne 0 ]
+then
+    echo "nginx was not install" 1>&2;
+    exit 1
+fi
+
 if [ "$pid" = "" ]
 then
 	echo "start orange.."
 else
 	echo "kill "$pid
 	#kill -s QUIT $pid
-	nginx -p `pwd` -c ./conf/nginx.conf -s stop
+	$nginx -p `pwd` -c ./conf/nginx.conf -s stop
 	echo "restart orange.."
 fi
 
 mkdir -p logs
-nginx -p `pwd` -c ./conf/nginx.conf
+$nginx -p `pwd` -c ./conf/nginx.conf
