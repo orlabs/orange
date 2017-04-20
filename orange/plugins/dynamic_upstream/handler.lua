b0e365f9c5557e5f4d3a627e29d988131b25931a
yang.zhenzhen <soul11201@gmail.com>
2017-04-20 15:01:47 +0800

    fix&refactor:  modify the orange/plugins code copied from rewrite plugiins to achieve the dynamic upstream feature

--- orange/plugins/dynamic_upstream/handler.lua
+++ orange/plugins/dynamic_upstream/handler.lua
@@ -8,13 +8,20 @@ local judge_util = require("orange.utils.judge")
 local extractor_util = require("orange.utils.extractor")
 local handle_util = require("orange.utils.handle")
 local BasePlugin = require("orange.plugins.base_handler")
-local ngx_set_uri = ngx.req.set_uri
 local ngx_set_uri_args = ngx.req.set_uri_args
 local ngx_decode_args = ngx.decode_args
 
+local function ngx_set_uri(uri,rule_handle)
+
+    ngx.var.upstream_request_uri = '/'..uri .. '?' ..  ngx.encode_args(ngx.req.get_uri_args())
+    ngx.var.upstream_name = rule_handle.upstream_name
+    ngx.log(ngx.INFO,'[DynamicUpstream][upstream uri][http://',ngx.var.upstream_name,ngx.var.upstream_request_uri,']')
+
+end
 
 local function filter_rules(sid, plugin, ngx_var_uri)
     local rules = orange_db.get_json(plugin .. ".selector." .. sid .. ".rules")
+
     if not rules or type(rules) ~= "table" or #rules <= 0 then
         return false
     end
@@ -22,18 +29,18 @@ local function filter_rules(sid, plugin, ngx_var_uri)
     for i, rule in ipairs(rules) do
         if rule.enable == true then
             -- judge阶段
-            local pass = judge_util.judge_rule(rule, "rewrite")
+            local pass = judge_util.judge_rule(rule, "dynamic_upstream")
             -- extract阶段
             local variables = extractor_util.extract_variables(rule.extractor)
 
             -- handle阶段
             if pass then
                 local handle = rule.handle
-                if handle and handle.uri_tmpl then
+                if handle and handle.uri_tmpl and handle.upstream_name then
                     local to_rewrite = handle_util.build_uri(rule.extractor.type, handle.uri_tmpl, variables)
                     if to_rewrite and to_rewrite ~= ngx_var_uri then
                         if handle.log == true then
-                            ngx.log(ngx.INFO, "[Rewrite] ", ngx_var_uri, " to:", to_rewrite)
+                            ngx.log(ngx.INFO, "[DynamicUpstream] ", ngx_var_uri, " to:", to_rewrite)
                         end
 
                         local from, to, err = ngx_re_find(to_rewrite, "[%?]{1}", "jo")
@@ -47,7 +54,8 @@ local function filter_rules(sid, plugin, ngx_var_uri)
                                 end
                             end
                         end
-                        ngx_set_uri(to_rewrite, true)
+                        ngx_set_uri(to_rewrite, handle)
+                        return true
                     end
                 end
 
@@ -63,16 +71,16 @@ local DynamicUpstreamHandler = BasePlugin:extend()
 DynamicUpstreamHandler.PRIORITY = 2000
 
 function DynamicUpstreamHandler:new(store)
-    RewriteHandler.super.new(self, "rewrite-plugin")
+    DynamicUpstreamHandler.super.new(self, "dynamic-upstream-plugin")
     self.store = store
 end
 
 function DynamicUpstreamHandler:rewrite(conf)
     DynamicUpstreamHandler.super.rewrite(self)
 
-    local enable = orange_db.get("rewrite.enable")
-    local meta = orange_db.get_json("rewrite.meta")
-    local selectors = orange_db.get_json("rewrite.selectors")
+    local enable = orange_db.get("dynamic_upstream.enable")
+    local meta = orange_db.get_json("dynamic_upstream.meta")
+    local selectors = orange_db.get_json("dynamic_upstream.selectors")
     local ordered_selectors = meta and meta.selectors
     
     if not enable or enable ~= true or not meta or not ordered_selectors or not selectors then
@@ -81,28 +89,28 @@ function DynamicUpstreamHandler:rewrite(conf)
 
     local ngx_var_uri = ngx.var.uri
     for i, sid in ipairs(ordered_selectors) do
-        ngx.log(ngx.INFO, "==[Rewrite][PASS THROUGH SELECTOR:", sid, "]")
         local selector = selectors[sid]
+        ngx.log(ngx.INFO, "==[DynamicUpstream][START SELECTOR:", sid, "][NAME:",selector.name,']')
         if selector and selector.enable == true then
             local selector_pass 
             if selector.type == 0 then -- 全流量选择器
                 selector_pass = true
             else
-                selector_pass = judge_util.judge_selector(selector, "rewrite")-- selector judge
+                selector_pass = judge_util.judge_selector(selector, "dynamic_upstream")-- selector judge
             end
 
             if selector_pass then
                 if selector.handle and selector.handle.log == true then
-                    ngx.log(ngx.INFO, "[Rewrite][PASS-SELECTOR:", sid, "] ", ngx_var_uri)
+                    ngx.log(ngx.INFO, "[DynamicUpstream][PASS-SELECTOR:", sid, "] ", ngx_var_uri)
                 end
 
-                local stop = filter_rules(sid, "rewrite", ngx_var_uri)
+                local stop = filter_rules(sid, "dynamic_upstream", ngx_var_uri)
                 if stop then -- 不再执行此插件其他逻辑
                     return
                 end
             else
                 if selector.handle and selector.handle.log == true then
-                    ngx.log(ngx.INFO, "[Rewrite][NOT-PASS-SELECTOR:", sid, "] ", ngx_var_uri)
+                    ngx.log(ngx.INFO, "[DynamicUpstream][NOT-PASS-SELECTOR:", sid, "] ", ngx_var_uri)
                 end
             end
 
