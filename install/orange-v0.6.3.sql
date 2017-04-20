baf44ac38313cbf7b1e7e186ba9a09388b5863dd
yang.zhenzhen <soul11201@gmail.com>
2017-04-20 16:11:56 +0800

    refactor:add sql

--- install/orange-v0.6.3.sql
+++ install/orange-v0.6.3.sql
@@ -290,6 +290,26 @@ VALUES
 /*!40000 ALTER TABLE `rewrite` ENABLE KEYS */;
 UNLOCK TABLES;
 
+# Dump of table dynamic_upstream
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `dynamic_upstream`;
+CREATE TABLE `dynamic_upstream` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `key` varchar(255) NOT NULL DEFAULT '',
+  `value` varchar(2000) NOT NULL DEFAULT '',
+  `type` varchar(11) DEFAULT '0',
+  `op_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_key` (`key`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+INSERT INTO `dynamic_upstream` (`id`, `key`, `value`, `type`, `op_time`)
+VALUES
+    (1,'1','{}','meta','2016-11-11 11:11:11');
+
+/*!40000 ALTER TABLE `dynamic_upstream` ENABLE KEYS */;
+UNLOCK TABLES;
+
 
 # Dump of table waf
 # ------------------------------------------------------------
