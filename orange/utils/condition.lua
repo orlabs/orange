083e221f1869e066f935a0412ebd3aee58fda3a8
soul11201 <soul11201@gmail.com>
2018-09-28 13:52:10 +0800

    Random

--- orange/utils/condition.lua
+++ orange/utils/condition.lua
@@ -90,7 +90,7 @@ function _M.judge(condition)
         real = headers[condition.name]
     elseif condition_type == "IP" then
         real =  ngx.var.remote_addr
-    elseif condition_type == "Weight" then
+    elseif condition_type == "Random" then
         real = ngx.now() * 1000 % 100
     elseif condition_type == "UserAgent" then
         real =  ngx.var.http_user_agent
