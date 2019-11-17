ce8b786a3614acfa8810238051d538785d73eddf
Janko <shuaijinchao@gmail.com>
2019-11-17 22:50:53 +0800

    transfer: remove persist plugin from version 0.8.0

--- orange/plugins/persist/api.lua
+++
@@ -1,7 +0,0 @@
-local BaseAPI = require("orange.plugins.base_api")
-local common_api = require("orange.plugins.common_api")
-
-local api = BaseAPI:new("persist-api", 2)
-api:merge_apis(common_api("persist"))
-
-return api
