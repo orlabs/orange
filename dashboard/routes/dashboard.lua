a2e230de46a0564fc6e942230ced6b8e169fbd32
Janko <shuaijinchao@gmail.com>
2019-11-18 11:21:42 +0800

    transfer: remove `dynamic_upstream` plugin dashboard.

--- dashboard/routes/dashboard.lua
+++ dashboard/routes/dashboard.lua
@@ -132,23 +132,6 @@ return function(config, store)
     dashboard_router:get("/redirect", function(req, res, next)
         res:render("redirect")
     end)
-    dashboard_router:get("/dynamic_upstream", function(req, res, next)
-        local upstream = require "ngx.upstream"
-
-        local upstream_list = upstream.get_upstreams()
-        local empty_table = false
-
-        if lua_next(upstream_list) == nil then
-            empty_table = true
-        end
-
-        local every_upstream_config = {}
-        for _, v in ipairs(upstream_list) do
-            every_upstream_config[v] = upstream.get_servers(v)
-        end
-
-        res:render("dynamic_upstream",{upstreams=upstream_list, empty_table = empty_table, every_upstream_config = json.encode(every_upstream_config)})
-    end)
     dashboard_router:get("/rate_limiting", function(req, res, next)
         res:render("rate_limiting")
     end)
