6de542dd1067a270d0fd83b9492b2d75fdc3d10e
xiaowu <943915349@qq.com>
2023-06-22 17:06:54 +0800

    update

--- orange/plugins/base_redis.lua
+++ orange/plugins/base_redis.lua
@@ -73,10 +73,10 @@ function BaseRedis.incr(cache_prefix, key, delta, ttl)
     key = cache_prefix .. ":" .. key
     local res, err
     if ttl then
-        res, err = cache:incr(key, delta or 1)
+        res, err = cache:incr(key)
         cache:expire(key, ttl)
     else
-        res, err = cache:incr(key, delta or 1)
+        res, err = cache:incr(key)
     end
     if err then
         ngx.log(ngx.ERR, "failed to incr Redis key: ", err)
