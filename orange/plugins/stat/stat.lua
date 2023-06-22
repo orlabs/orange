2a96d44b9698a3319632a1a88c3daa8921d331d6
xiaowu <943915349@qq.com>
2023-06-22 11:50:58 +0800

    update

--- orange/plugins/stat/stat.lua
+++ orange/plugins/stat/stat.lua
@@ -22,7 +22,7 @@ local _M = {}
 
 function _M.init()
     local res, _ = redis.get_string(status, STAT_LOCK)
-    ngx.log(ngx.ERR, res)
+    ngx.log(ngx.ERR, not res)
     if not res then
         redis.set(status, STAT_LOCK, true)
         -- ngx.time() 是 OpenResty 提供的一个函数，用于获取当前时间戳。它返回的是一个整数
